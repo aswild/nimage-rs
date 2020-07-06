4d2d5435a2513e63343b8b3901dd6d8e5afb397f
Allen Wild <allenwild93@gmail.com>
2020-07-05 22:27:58 -0400

    swdl: initial skeleton
    Add swdl binary code and the Input wrapper for stdin/file/curl. The
    current "swdl" binary basically just behaves like cat.
    
    I tried messing with the curl library and looked at other HTTP client
    crates, but all of it seems to either be fully synchronous requests,
    a bunch of async futures stuff, or a bunch of callbacks. Any network
    code inherently has to be threaded or async, but I'm not ready to dive
    down that rabbit hole yet (though it could be a good learning exercise
    some day).
    
    Thus, use the same strategy as C++ newbs-swdl and fork off a curl
    process and just read from its stdout pipe, letting the OS handle all
    the intermediate buffering and blocking and task switching.

---
+++ src/swdl/input.rs
@@ -0,0 +1,120 @@
+/*!
+ * swdl: Raspberry Pi firmware update engine.
+ * input reader
+ *
+ * Copyright 2020 Allen Wild
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+use std::fmt;
+use std::fs::File;
+use std::io::{self, BufReader, Read};
+use std::os::unix::process::ExitStatusExt;
+use std::process::{Child, Command, Stdio};
+
+use crate::*;
+
+#[derive(Debug)]
+pub struct FileInfo {
+    // path as a string for easier printing. Technically should be a PathBuf
+    path: String,
+    // bufreader of the open file object
+    file: BufReader<File>,
+}
+
+#[derive(Debug)]
+pub struct CurlInfo {
+    url: String,
+    child: Child,
+}
+
+#[derive(Debug)]
+pub enum Input {
+    Stdin(BufReader<io::Stdin>),
+    File(FileInfo),
+    Curl(CurlInfo),
+}
+
+impl Input {
+    pub fn new(path: &str) -> Result<Self> {
+        // easy case, reading stdin
+        if path == "-" {
+            debug!("opening stdin");
+            return Ok(Input::Stdin(BufReader::new(io::stdin())));
+        }
+
+        // try to open path as a local file
+        match File::open(path) {
+            // sucess
+            Ok(file) => {
+                debug!("opened {} as a local file", path);
+                Ok(Input::File(FileInfo { path: path.to_string(), file: BufReader::new(file) }))
+            }
+
+            // couldn't open as a file, do it as a piped curl command
+            Err(_) => {
+                let child = Command::new("curl")
+                    .arg("-sSLf")
+                    .arg("--netrc")
+                    .arg("--")
+                    .arg(path)
+                    .stdout(Stdio::piped())
+                    .spawn()?;
+                debug!("downloading {} with curl", path);
+                Ok(Input::Curl(CurlInfo { url: path.to_string(), child }))
+            }
+        }
+    }
+}
+
+impl fmt::Display for Input {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.write_str(match self {
+            Input::Stdin(_) => "[standard input]",
+            Input::File(info) => &info.path,
+            Input::Curl(info) => &info.url,
+        })
+    }
+}
+
+impl Read for Input {
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        match self {
+            // stdin and a local file are easy
+            Input::Stdin(r) => r.read(buf),
+            Input::File(info) => info.file.read(buf),
+
+            // curl pipe is trickier
+            Input::Curl(info) => {
+                // try the read, immediately returning any read error
+                let count = info.child.stdout.as_mut().unwrap().read(buf)?;
+                if count != 0 {
+                    // we read some bytes
+                    return Ok(count);
+                }
+
+                // we read nothing, which means curl is done, check its return status.
+                // wait() could return an error but that shouldn't happen
+                let status = info.child.wait().expect("failed to wait for curl process");
+                if status.success() {
+                    Ok(0)
+                } else if let Some(code) = status.code() {
+                    // normal non-successful exit
+                    Err(io::Error::new(
+                        io::ErrorKind::Other,
+                        format!("curl process exited with status {}", code),
+                    ))
+                } else if let Some(sig) = status.signal() {
+                    // killed by a signal
+                    Err(io::Error::new(
+                        io::ErrorKind::Other,
+                        format!("curl process killed with signal {}", sig),
+                    ))
+                } else {
+                    // should never get here
+                    panic!("curl process exited in an unknown fashion!")
+                }
+            }
+        }
+    }
+}
---
+++ src/swdl/main.rs
@@ -0,0 +1,77 @@
+/*!
+ * swdl: Raspberry Pi firmware update engine.
+ * main executable
+ *
+ * Copyright 2020 Allen Wild
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+mod input;
+
+use std::process::exit;
+
+pub use anyhow::{anyhow, Context, Result};
+pub use clap::ArgMatches;
+use clap::{crate_version, App, AppSettings, Arg};
+
+use input::Input;
+
+// copied from mknImage/main.rs
+// TODO: use the log crate or something cleaner
+static mut DEBUG_ENABLED: bool = false;
+#[macro_export]
+macro_rules! debug {
+    ($($arg:tt)*) => {
+        if unsafe { DEBUG_ENABLED } {
+            let mut filename = file!();
+            if filename.starts_with("src/") {
+                filename = &filename[4..];
+            }
+            if filename.ends_with(".rs") {
+                filename = &filename[..(filename.len()-3)];
+            }
+            eprint!("[DEBUG {}:{}] ", filename, line!());
+            eprintln!($($arg)*);
+        }
+    }
+}
+
+fn do_swdl(url: &str) -> Result<()> {
+    let mut rx = Input::new(url)?;
+    std::io::copy(&mut rx, &mut std::io::stdout())?;
+    Ok(())
+}
+
+fn main() {
+    #[rustfmt::skip]
+    let args = App::new("newbs-swdl")
+        .version(crate_version!())
+        .about("RPi Software Download")
+        .max_term_width(100)
+        .global_setting(AppSettings::ColoredHelp)
+        .arg(
+            Arg::with_name("debug")
+                .short("D")
+                .long("debug")
+                .help("Enable extra debug output")
+        )
+        .arg(
+            Arg::with_name("url")
+                .required(true)
+                .value_name("IMAGE FILE/URL")
+                .help("Image to download. Can be a local file path, URL, or '-' for stdin"),
+        )
+        .get_matches();
+
+    unsafe {
+        DEBUG_ENABLED = args.is_present("debug");
+    }
+    debug!("debug logging enabled");
+
+    if let Err(err) = do_swdl(args.value_of("url").unwrap()) {
+        if !err.to_string().is_empty() {
+            eprintln!("Error: {:#}", err);
+        }
+        exit(1);
+    }
+}
